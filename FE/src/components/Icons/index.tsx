0e7b5a3d401b57ca561ae9dc748fdef24719c48f
Cola <bcad1591@gmail.com>
2022-06-14 17:22:22 +0900

    style: #7 아이콘 컴포넌트 내보내기 추가

---
+++ FE/src/components/Icons/index.tsx
@@ -0,0 +1,11 @@
+import CheckBox from './modules/CheckBox';
+import Circle from './modules/Circle';
+import * as Etc from './modules/Etc';
+import Logo from './modules/Logo';
+
+export default {
+  CheckBox,
+  Circle,
+  Logo,
+  ...Etc,
+};
