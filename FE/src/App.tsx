3aba53fa200e9299d9e5f99752b04bdc82fe129d
Cola <bcad1591@gmail.com>
2022-06-14 17:31:50 +0900

    [FE] Style: 아이콘 폰트 컴포넌트
    * chore: #7 프로젝트 추가 환경 설정
    
    * style: #7 Icon Fonts 스타일 수정, 아이콘 컴포넌트 공통 스타일 추가
    
    * chore: #7 eslint-plugin-prettier 추가
    
    * style: #7 아이콘 컴포넌트 공통 스타일 추가
    
    * style: #7 Logo 아이콘 컴포넌트 추가
    
    * style: #7 CheckBox 아이콘 컴포넌트 추가
    
    * style: #7 Circle 아이콘 컴포넌트 추가
    
    * style: #7 기타 아이콘 컴포넌트 추가
    
    * style: #7 아이콘 컴포넌트 내보내기 추가
    
    * style: #7 아이콘 컴포넌트 테스트용 페이지 추가

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
 
+import IconPage from './IconPage';
+
 function App() {
   return (
     <div>
       <h1>ISSUE TRACKER</h1>
+      <IconPage />
     </div>
   );
 }
