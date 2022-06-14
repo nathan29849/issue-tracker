bda71e06f5dd9f8216fd226f5ff8808e08c6bec8
Cola <bcad1591@gmail.com>
2022-06-14 17:22:09 +0900

    style: #7 기타 아이콘 컴포넌트 추가

---
+++ FE/src/components/Icons/modules/Etc.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+
+import * as S from './style';
+
+interface Props {
+  fontSize?: number;
+  color?: string;
+}
+
+export function Search(props: Props) {
+  return <S.Icon className="ic-search" {...props} />;
+}
+
+export function Smile(props: Props) {
+  return <S.Icon className="ic-smile" {...props} />;
+}
+
+export function Plus(props: Props) {
+  return <S.Icon className="ic-plus" {...props} />;
+}
+
+export function XMark(props: Props) {
+  return <S.Icon className="ic-x-mark" {...props} />;
+}
+
+export function Tag(props: Props) {
+  return <S.Icon className="ic-tag" {...props} />;
+}
+
+export function MileStone(props: Props) {
+  return <S.Icon className="ic-milestone" {...props} />;
+}
+
+export function Bucket(props: Props) {
+  return <S.Icon className="ic-archive" {...props} />;
+}
+
+export function Edit(props: Props) {
+  return <S.Icon className="ic-edit" {...props} />;
+}
+
+export function Trash(props: Props) {
+  return <S.Icon className="ic-trash" {...props} />;
+}
+
+export function Refresh(props: Props) {
+  return <S.Icon className="ic-refresh" {...props} />;
+}
+
+export function Calendar(props: Props) {
+  return <S.Icon className="ic-calendar" {...props} />;
+}
+
+export function Clip(props: Props) {
+  return <S.Icon className="ic-paper-clip" {...props} />;
+}
