c1ebc14beac906b245093d148530f8b603c1ad15
Cola <bcad1591@gmail.com>
2022-06-15 21:10:00 +0900

    design: #4 Layout 컴포넌트 기본 스타일 추가

--- FE/src/components/Layout.tsx
+++ FE/src/components/Layout.tsx
@@ -1,10 +1,17 @@
+import { css } from '@emotion/react';
 import { Outlet } from 'react-router-dom';
 
 import Header from '@components/Header';
 
+const containerStyle = css`
+  width: 90rem;
+  padding: 0 5rem;
+  margin: 0 auto;
+`;
+
 export default function Layout() {
   return (
-    <div>
+    <div css={containerStyle}>
       <Header />
       <Outlet />
     </div>
