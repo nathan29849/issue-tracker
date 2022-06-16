30498aeb1d0e6a75dbef46642f08804491f92a25
Cola <bcad1591@gmail.com>
2022-06-16 13:48:17 +0900

    feat: #6 로그인 버튼에 onClick 추가
    - /issue로 라우팅 (임시)

--- FE/src/pages/Login/index.tsx
+++ FE/src/pages/Login/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigate } from 'react-router-dom';
+
 import * as S from './style';
 
 import { LoginButton, TextButton } from '@components/Button';
@@ -5,6 +7,8 @@ import I from '@components/Icons';
 import theme from '@styles/theme';
 
 export default function Login() {
+  const navigate = useNavigate();
+
   return (
     <S.LoginPageLayer>
       <S.LogoLayer>
@@ -15,10 +19,19 @@ export default function Login() {
         <LoginButton
           bgColor={theme.color.titleActive}
           textColor={theme.color.offWhite}
+          onClick={() => {
+            navigate('/issue');
+          }}
         >
           GitHub 계정으로 로그인
         </LoginButton>
-        <LoginButton bgColor="#f9e000" textColor="#181600">
+        <LoginButton
+          bgColor="#f9e000"
+          textColor="#181600"
+          onClick={() => {
+            navigate('/issue');
+          }}
+        >
           Kakao 계정으로 로그인
         </LoginButton>
       </S.OAuthLayer>
