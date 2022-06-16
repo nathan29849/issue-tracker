2a79519ff2ff7a244402f1873a6666da1abea458
Cola <bcad1591@gmail.com>
2022-06-16 13:13:31 +0900

    refactor: #4 버튼 컴포넌트 인터페이스 수정
    - React.ComponentPropsWithoutRef 적용
    - disabled 삭제(이미 위에 포함되기 때문에)

--- FE/src/components/Button/index.tsx
+++ FE/src/components/Button/index.tsx
@@ -2,23 +2,25 @@ import React from 'react';
 
 import * as S from './style';
 
-export const Button: React.FC<{
-  children: React.ReactNode;
-  outlined?: boolean;
-  size?: 'sm' | 'md' | 'lg';
-  disabled?: boolean;
-}> = ({ children, outlined = false, size = 'sm', disabled = false }) => (
-  <S.Button type="button" disabled={disabled} outlined={outlined} size={size}>
+export const Button: React.FC<
+  {
+    children: React.ReactNode;
+    outlined?: boolean;
+    size?: 'sm' | 'md' | 'lg';
+  } & React.ComponentPropsWithoutRef<'button'>
+> = ({ children, outlined = false, size = 'sm', ...rest }) => (
+  <S.Button type="button" outlined={outlined} size={size} {...rest}>
     <span>{children}</span>
   </S.Button>
 );
 
-export const TextButton: React.FC<{
-  children: React.ReactNode;
-  size?: 'sm' | 'md';
-  disabled?: boolean;
-}> = ({ children, size = 'sm', disabled = false }) => (
-  <S.TextButton size={size} disabled={disabled}>
+export const TextButton: React.FC<
+  {
+    children: React.ReactNode;
+    size?: 'sm' | 'md';
+  } & React.ComponentPropsWithoutRef<'button'>
+> = ({ children, size = 'sm', ...rest }) => (
+  <S.TextButton size={size} {...rest}>
     {children}
   </S.TextButton>
 );
