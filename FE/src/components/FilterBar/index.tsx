21b572bbae59e9f49a8c1a0723ff87561fb813a1
Muffin <jinlog9@gmail.com>
2022-06-17 00:08:24 +0900

    feat: 검색창 이슈 필터 팝업 구현 진행중

--- FE/src/components/FilterBar/index.tsx
+++ FE/src/components/FilterBar/index.tsx
@@ -1,14 +1,58 @@
-import React from 'react';
-
 import * as S from './style';
+import Popup from '@components/Popup/Popup';
+import useComponentVisible from '@hooks/useComponentVisible.jsx';
+import { getModalItem } from '@components/Popup/Content';
 
 import I from '@components/Icons';
 
 export default function FilterBar() {
+  const issueFilterData = {
+    info: [
+      { status: 'open', name: '열린 이슈' },
+      { status: 'mine', name: '내가 작성한 이슈' },
+      { status: 'assignedToMe', name: '나에게 할당된 이슈' },
+      { status: 'comment', name: '내가 댓글을 남긴 이슈' },
+      { status: 'close', name: '닫힌 이슈' },
+    ],
+  };
+  const callback = () => {
+    console.log('데이터 필터되는 콜백함수');
+  };
+  const { ref, isComponentVisible, setIsComponentVisible } =
+    useComponentVisible(false, callback);
+
+  const handleOnFilterPopup = () => {
+    setIsComponentVisible(true);
+  };
+
   return (
     <S.FilterBarLayer>
-      <S.FilterButton>
+      <S.FilterButton onClick={handleOnFilterPopup}>
         <span>필터</span>
+        <div ref={ref}>
+          {isComponentVisible && (
+            <Popup>
+              <header>
+                {'이슈'} {'필터'}
+              </header>
+              {issueFilterData.info.map((popupData: any) => {
+                return (
+                  <div
+                    className="filter__item-wrapper"
+                    key={`${popupData.status}`}
+                  >
+                    <div className="filter__item">
+                      {getModalItem('이슈', popupData)}
+                    </div>
+                    <div className="filter__check">
+                      <I.Circle.Check />
+                    </div>
+                  </div>
+                );
+              })}
+            </Popup>
+          )}
+        </div>
         <I.ArrowDown />
       </S.FilterButton>
       <S.SearchBar>
