b018620927107302080d01caccfe7422a2e0cc72
Cola <bcad1591@gmail.com>
2022-06-16 10:58:09 +0900

    feat: Buttons 페이지에 TextButton 업데이트

--- FE/src/pages/ButtonPage/index.tsx
+++ FE/src/pages/ButtonPage/index.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@components/Button';
+import { Button, TextButton } from '@components/Button';
 import I from '@components/Icons';
 
 export default function ButtonPage() {
@@ -8,7 +8,6 @@ export default function ButtonPage() {
         <legend>
           <h2>Normal Button</h2>
         </legend>
-
         <div>
           <Button>
             <I.Plus />
@@ -34,17 +33,44 @@ export default function ButtonPage() {
           </Button>
           <br />
         </div>
+        <div>
+          <Button size="lg" disabled>
+            Disabled Button
+          </Button>
+          <br />
+          <Button outlined size="lg" disabled>
+            Outlined Disabled Button
+          </Button>
+          <br />
+        </div>
       </fieldset>
 
       <fieldset style={{ display: 'inline-block' }}>
         <legend>
-          <h2>Normal Button</h2>
+          <h2>Text Button</h2>
         </legend>
 
         <div>
-          <Button>Button</Button>
+          <TextButton>
+            <I.Plus />
+            Small Button
+          </TextButton>
           <br />
-          <Button outlined>Outlined Button</Button>
+          <TextButton disabled>
+            <I.XMark />
+            Small Disabled Button
+          </TextButton>
+          <br />
+          <TextButton size="md">
+            <I.Plus />
+            Medium Button
+          </TextButton>
+          <br />
+          <TextButton size="md" disabled>
+            <I.XMark />
+            Medium Disabled Button
+          </TextButton>
+          <br />
         </div>
       </fieldset>
     </>
