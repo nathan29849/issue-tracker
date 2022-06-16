a4963932c239bb6cd2c4addf15477d836960cf20
Cola <bcad1591@gmail.com>
2022-06-16 19:14:06 +0900

    refactor: App 컴포넌트 단순화
    - 이슈 페이지 등 따로 분리하였으므로 App 컴포넌트를 단순화

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 
-import IconPage from './IconPage';
 import Router from './Router';
 
 function App() {
   return (
     <div>
       <Router />
-      <h1>ISSUE TRACKER</h1>
-      <IconPage />
     </div>
   );
 }
