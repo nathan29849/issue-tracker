2568b2a61ecf274386c0c32597675c1bd0de6a77
Cola <bcad1591@gmail.com>
2022-06-16 21:34:59 +0900

    [FE] Login Page UI
    * feat: #6 로그인 페이지 UI 구현
    
    * feat: #6 로그인 버튼에 onClick 추가
    
    - /issue로 라우팅 (임시)
    
    * feat: #4 utility types 추가
    
    - 재사용 가능한 컴포넌트를 만들기 위해 필요
    
    * refactor: #4 UserAvatar 분리
    
    * refactor: #6 User Avatar 수정
    
    - tag type을 button으로 바꿈
    
    * refactor: #4 버튼 컴포넌트 interface 변경
    
    - util type을 이용하여 인터페이스 변경
    - 재사용 가능한 버튼 만들기
    
    * refactor: App 컴포넌트 단순화
    
    - 이슈 페이지 등 따로 분리하였으므로 App 컴포넌트를 단순화
    
    * refactor: Button Page 업데이트

---
+++ FE/src/components/UserAvatar/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import * as S from './style';
+
+import { OverridableProps } from '@utils/types';
+
+interface Props {
+  src: string;
+  size?: 'sm' | 'lg';
+  alt?: string;
+}
+
+// T라는 리액트 엘리먼트의 props와 위에 정의한 Props의 props, as props를 가지는 type
+type UserAvatarProps<T extends React.ElementType> = OverridableProps<T, Props>;
+
+const UserAvatar = <T extends React.ElementType = 'div'>({
+  alt = '',
+  size = 'sm',
+  src,
+  as,
+  ...restProps
+}: UserAvatarProps<T>) => (
+  <S.UserAvatar as={as ?? 'div'} size={size} {...restProps}>
+    <S.Image src={src} alt={alt} />
+  </S.UserAvatar>
+);
+
+export default UserAvatar;
--- FE/src/pages/Login/index.tsx
+++ FE/src/pages/Login/index.tsx
@@ -1,3 +1,58 @@
+import { useNavigate } from 'react-router-dom';
+
+import * as S from './style';
+
+import { LoginButton, TextButton } from '@components/Button';
+import I from '@components/Icons';
+import theme from '@styles/theme';
+
 export default function Login() {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+
+  return (
+    <S.LoginPageLayer>
+      <S.LogoLayer>
+        <I.Logo.Big />
+      </S.LogoLayer>
+
+      <S.OAuthLayer>
+        <LoginButton
+          bgColor={theme.color.titleActive}
+          textColor={theme.color.offWhite}
+          onClick={() => {
+            navigate('/issue');
+          }}
+        >
+          GitHub 계정으로 로그인
+        </LoginButton>
+        <LoginButton
+          bgColor="#f9e000"
+          textColor="#181600"
+          onClick={() => {
+            navigate('/issue');
+          }}
+        >
+          Kakao 계정으로 로그인
+        </LoginButton>
+      </S.OAuthLayer>
+
+      <TextButton size="sm" disabled css={S.CSSTextButtonMargin}>
+        or
+      </TextButton>
+
+      <S.NormalLoginLayer>
+        <LoginButton
+          bgColor={theme.color.blue}
+          textColor={theme.color.offWhite}
+          disabled
+        >
+          아이디로 로그인
+        </LoginButton>
+      </S.NormalLoginLayer>
+
+      <TextButton size="sm" css={S.CSSTextButtonMargin}>
+        회원가입
+      </TextButton>
+    </S.LoginPageLayer>
+  );
 }
