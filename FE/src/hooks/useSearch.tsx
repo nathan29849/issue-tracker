b67731b64f487ea782c52019f70ae395cf2d5e59
Cola <bcad1591@gmail.com>
2022-06-18 23:55:33 +0900

    refactor: #37 useSearch Hook 리팩토링
    변수 네이밍
    - key -> paramKey
    - searchValue -> paramValue

--- FE/src/hooks/useSearch.tsx
+++ FE/src/hooks/useSearch.tsx
@@ -1,9 +1,9 @@
 import { useSearchParams } from 'react-router-dom';
 
 // 서치 파라미터중 특정 키에 해당하는 값을 반환하는 Hook
-export const useSearch = (key: string, defaultValue: string) => {
+export const useSearch = (paramKey: string, defaultValue: string) => {
   const [searchParams] = useSearchParams();
-  const searchValue = searchParams.get(key);
+  const paramValue = searchParams.get(paramKey);
 
-  return searchValue ?? defaultValue;
+  return paramValue ?? defaultValue;
 };
