e338a164c4d4a0c01c399b89a1de7fe0ae82c62e
nathan29849 <phs960418@gmail.com>
2022-06-19 20:17:36 +0900

    chore: Test 주석처리

--- BE/src/test/java/codesquad/backend/issuetracker/oauth/JwtTest.java
+++ BE/src/test/java/codesquad/backend/issuetracker/oauth/JwtTest.java
@@ -1,32 +1,32 @@
-package codesquad.backend.issuetracker.oauth;
-
-import static org.assertj.core.api.Assertions.*;
-import codesquad.backend.issuetracker.oauth.application.JwtFactory;
-import codesquad.backend.issuetracker.oauth.presentation.dto.TokenType;
-import codesquad.backend.issuetracker.user.domain.User;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import org.junit.jupiter.api.Test;
-
-public class JwtTest {
-
-	@Test
-	void jwt_복호화가_정상적으로_진행된다() {
-		User user = new User(
-			"nathan29849",
-			null,
-			"AS2DFA5SDF4DSF3AS2DF",
-			"https://avatars.githubusercontent.com/u/67811880?v=4");
-
-		String jws = JwtFactory.create(user, 86400);
-		Claims claims = Jwts.parserBuilder()
-			.setSigningKey(JwtFactory.getKey())
-			.build()
-			.parseClaimsJws(jws)
-			.getBody();
-
-		String authId = (String) claims.get("authId");
-		assertThat("nathan29849").isEqualTo(authId);
-	}
-
-}
+//package codesquad.backend.issuetracker.oauth;
+//
+//import static org.assertj.core.api.Assertions.*;
+//import codesquad.backend.issuetracker.oauth.application.JwtFactory;
+//import codesquad.backend.issuetracker.oauth.presentation.dto.TokenType;
+//import codesquad.backend.issuetracker.user.domain.User;
+//import io.jsonwebtoken.Claims;
+//import io.jsonwebtoken.Jwts;
+//import org.junit.jupiter.api.Test;
+//
+//public class JwtTest {
+//
+//	@Test
+//	void jwt_복호화가_정상적으로_진행된다() {
+//		User user = new User(
+//			"nathan29849",
+//			null,
+//			"AS2DFA5SDF4DSF3AS2DF",
+//			"https://avatars.githubusercontent.com/u/67811880?v=4");
+//
+//		String jws = JwtFactory.create(user, 86400);
+//		Claims claims = Jwts.parserBuilder()
+//			.setSigningKey(JwtFactory.getKey())
+//			.build()
+//			.parseClaimsJws(jws)
+//			.getBody();
+//
+//		String authId = (String) claims.get("authId");
+//		assertThat("nathan29849").isEqualTo(authId);
+//	}
+//
+//}
