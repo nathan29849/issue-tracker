19a7f333abe54b5d127f84034d4c02c09fa9c100
nathan29849 <phs960418@gmail.com>
2022-06-20 21:02:33 +0900

    feat: [49] Spring Interceptor 관련 설정 추가 및 기능 구현 (Access, Refresh 나눠서)

---
+++ BE/src/main/java/codesquad/backend/issuetracker/oauth/interceptor/AuthInterceptor.java
@@ -0,0 +1,29 @@
+package codesquad.backend.issuetracker.oauth.interceptor;
+
+import codesquad.backend.issuetracker.exception.AuthException;
+import codesquad.backend.issuetracker.exception.ErrorCode;
+import codesquad.backend.issuetracker.oauth.application.JwtFactory;
+import codesquad.backend.issuetracker.oauth.application.OAuthService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class AuthInterceptor extends CommonInterceptor {
+
+	public AuthInterceptor(OAuthService oAuthService) {
+		super(oAuthService);
+	}
+
+	@Override
+	protected Claims getClaims(String authorizationHeader) {
+		Claims claims;
+		try {
+			claims = JwtFactory.parseClaims(authorizationHeader.substring("Bearer " .length()));
+		} catch (ExpiredJwtException e) {
+			throw new AuthException(ErrorCode.ACCESS_TOKEN_EXPIRED);
+		}
+		return claims;
+	}
+
+}
---
+++ BE/src/main/java/codesquad/backend/issuetracker/oauth/interceptor/CommonInterceptor.java
@@ -0,0 +1,49 @@
+package codesquad.backend.issuetracker.oauth.interceptor;
+
+import codesquad.backend.issuetracker.exception.AuthException;
+import codesquad.backend.issuetracker.exception.ErrorCode;
+import codesquad.backend.issuetracker.oauth.application.OAuthService;
+import codesquad.backend.issuetracker.user.domain.User;
+import io.jsonwebtoken.Claims;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Slf4j
+@RequiredArgsConstructor
+public abstract class CommonInterceptor implements HandlerInterceptor {
+
+	private final OAuthService oAuthService;
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+		Object handler) throws Exception {
+
+		String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+		tokenValidationCheck(authorizationHeader);
+		Claims claims = getClaims(authorizationHeader);
+		User user = userValidationCheck(claims);
+		request.setAttribute("user", user);
+
+		return true;
+	}
+
+	protected void tokenValidationCheck(String authorizationHeader) {
+		if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")){
+			throw new AuthException(ErrorCode.UNAVAILABLE_TOKEN);
+		}
+	}
+
+	protected User userValidationCheck(Claims claims) {
+		String userSecret = (String) claims.get("userSecret");
+		return oAuthService.findByUserSecret(userSecret)
+			.orElseThrow(() -> new AuthException(ErrorCode.UNAUTHORIZED_USER));
+	}
+
+	protected Claims getClaims(String authorizationHeader){
+		throw new AuthException(ErrorCode.ACCESS_TOKEN_EXPIRED);
+	};
+}
