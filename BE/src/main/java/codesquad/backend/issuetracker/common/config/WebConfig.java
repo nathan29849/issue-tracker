966b9d83501ce085a95ab433740321d0bb320ed6
nathan29849 <phs960418@gmail.com>
2022-06-20 10:33:35 +0900

    add: CORS 관련 설정정보 추가

---
+++ BE/src/main/java/codesquad/backend/issuetracker/common/config/WebConfig.java
@@ -0,0 +1,19 @@
+package codesquad.backend.issuetracker.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("http://localhostL3000",
+				"http://3.36.69.201");
+
+	}
+}
