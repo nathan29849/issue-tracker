b31088223e7dbed10f7ec6ae5925a931f6703a49
Cola <bcad1591@gmail.com>
2022-06-22 14:26:46 +0900

    feat: #69 로그인 Loader 분리

--- FE/src/pages/Callback/index.tsx
+++ FE/src/pages/Callback/index.tsx
@@ -1,31 +1,33 @@
-import { css, keyframes } from '@emotion/react';
-import React from 'react';
+import { css } from '@emotion/react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useSetRecoilState } from 'recoil';
 
-import { alignPosXY, flexbox } from '@styles/mixin';
+import { githubLogin } from '../../apis/login';
+
+import { Loader } from '@components/Indicator/Loader';
+import { userState } from '@recoil/atoms/user';
 import theme from '@styles/theme';
-
-const spin = keyframes`
-  0% {
-    transform: translate3d(-50%, -50%, 0) rotate(0);
-  }
-  100% {
-    transform: translate3d(-50%, -50%, 0) rotate(360deg);
-  }
-`;
-
-const shake = keyframes`
-  from {
-    transform: translate3d(-50%, -50%, 0) rotate(3deg);
-  }
-  to {
-    transform: translate3d(-50%, -50%, 0) rotate(-3deg);
-  }
-`;
 
 function Callback() {
   const navigate = useNavigate();
   const { search } = window.location;
+  const setUser = useSetRecoilState(userState);
+
+  useEffect(() => {
+    const login = async (searchParams: string) => {
+      try {
+        const user = await githubLogin(searchParams);
+        console.log(user);
+        setUser(user);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        navigate('/');
+      }
+    };
+    login(search);
+  }, [search]);
 
   return (
     <div
@@ -34,29 +36,7 @@ function Callback() {
         background-color: ${theme.color.background};
       `}
     >
-      <div
-        css={css`
-          ${alignPosXY('fixed')};
-          ${flexbox({ jc: 'center', ai: 'center' })}
-          width: 15rem;
-          height: 15rem;
-          border: 0.5rem solid ${theme.color.titleActive};
-          border-top-color: transparent;
-          border-bottom-color: transparent;
-          border-radius: 50%;
-          animation: ${spin} 800ms infinite;
-        `}
-      />
-      <div
-        css={css`
-          ${alignPosXY('fixed')};
-          font-size: 1.5rem;
-          animation: ${shake} 100ms infinite;
-          color: ${theme.color.titleActive};
-        `}
-      >
-        로그인 중입니다...
-      </div>
+      <Loader text="로그인 중입니다.." size={16} />
     </div>
   );
 }
