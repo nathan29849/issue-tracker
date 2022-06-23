60f8c7a1f16bcd260bc517426ba7627e685b5b65
Muffin <jinlog9@gmail.com>
2022-06-23 15:30:03 +0900

    feat: Param 타입 지정,  issue 페이지에 바로 로드시 데이터 뿌려주기

--- FE/src/components/Issue/index.tsx
+++ FE/src/components/Issue/index.tsx
@@ -7,19 +7,24 @@ import { IssueWrapperLayer } from './style';
 
 import Item from '@components/Issue/Item';
 import Navigation from '@components/Issue/Navigation';
-import { issueState } from '@recoil/atoms/issue';
+import { IIssueTypes, issueState } from '@recoil/atoms/issue';
+
+interface IKeyParams {
+  [key: string]: string;
+}
 
 export default function Issue() {
-  const [issue, setIssue] = useRecoilState(issueState);
-  const [renderIssue, setRenderIssue] = useState(
-    issue.filter(is => is.status === 'open'),
-  );
+  const [initIssue, setInitIssue] = useRecoilState(issueState);
+  const [renderIssue, setRenderIssue] = useState<IIssueTypes[]>([]);
   const location = useLocation();
 
   useQuery('issueData', () => {
     fetch('issue').then(res => {
       res.json().then(result => {
-        setIssue(result);
+        setInitIssue(result);
+        setRenderIssue(
+          result.filter((issue: IIssueTypes) => issue.status === 'open'),
+        );
       });
     });
   });
@@ -28,12 +33,15 @@ export default function Issue() {
     const urlSearch = decodeURI(decodeURIComponent(location.search));
     const params = new URLSearchParams(urlSearch);
     const urlValues = params.get('q');
+
+    if (urlValues === null) return;
+
     const parsingUrlValue = urlValues?.split(' ').map(v => v);
 
     // 스페이스바 기준으로 나눠진 데이터들 중 다시 sep(:) split =>  Key, value를 담는 배열로 나누는 함수 제작
     // 뽑은 key, value를 넘겨 기존 issueData를 필터하는 함수 제작
     const filterDataArray = parsingUrlValue?.map(data => {
-      const obj = {};
+      const obj: IKeyParams = {};
       const [key, value] = data.split(':');
       obj[key] = value;
       return obj;
@@ -43,9 +51,12 @@ export default function Issue() {
     filterDataArray?.forEach(arr => {
       const key = Object.keys(arr)[0];
       const value = Object.values(arr)[0];
+
       switch (key) {
         case 'is':
-          setRenderIssue(issue.filter(is => is.status === value));
+          setRenderIssue(
+            initIssue.filter((issue: IIssueTypes) => issue.status === value),
+          );
           break;
         case 'mine':
           break;
@@ -65,13 +76,13 @@ export default function Issue() {
           console.log(urlValues);
       }
     });
-  }, [issue, location]);
+  }, [initIssue, location]);
 
   return (
     <IssueWrapperLayer>
       <Navigation />
       {renderIssue &&
-        renderIssue.map((issueData, index) => (
+        renderIssue.map((issueData: IIssueTypes, index: number) => (
           <Item
             issue={issueData}
             lastIdx={renderIssue.length === index + 1}
