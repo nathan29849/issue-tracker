c2881c9a76f500960e183495a9d1d441573790a0
Cola <bcad1591@gmail.com>
2022-06-24 00:11:32 +0900

    chore: 웹팩 publicPath 설정

--- FE/config/webpack.common.js
+++ FE/config/webpack.common.js
@@ -18,6 +18,7 @@ module.exports = {
     path: path.resolve(__dirname, '../build'),
     clean: true,
     assetModuleFilename: 'assets/[hash][ext][query]',
+    publicPath: '/'
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.jsx', '.js'],
