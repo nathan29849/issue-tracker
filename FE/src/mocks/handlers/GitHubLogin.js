e20f8d555a367ef7c3e00316186e7260e01bc987
Cola <bcad1591@gmail.com>
2022-06-26 03:16:29 +0900

    feat: GitHubLogin Mock API 추가

---
+++ FE/src/mocks/handlers/GitHubLogin.js
@@ -0,0 +1,45 @@
+import { rest } from 'msw';
+
+export const GitHubLogin = rest.get(
+  `${process.env.TEAM30_GITHUB_OAUTH_URL}/callback`,
+  (req, res, ctx) =>
+    res(
+      ctx.status(200),
+      ctx.delay(1500),
+      ctx.json({
+        userId: 'mjsdo',
+        accessToken: 'accessToken',
+        refreshToken: 'refreshToken',
+        profileImageUrl:
+          'https://avatars.githubusercontent.com/u/79135734?s=96&v=4',
+      }),
+    ),
+);
+
+export const RefreshGitHubLogin = rest.get(
+  `${process.env.TEAM30_GITHUB_OAUTH_URL}/refresh`,
+  (req, res, ctx) => {
+    const authorizationValue = req.headers.get('authorization').split(' ')[1];
+    if (authorizationValue !== 'refreshToken') {
+      return res(
+        ctx.status(400),
+        ctx.delay(500),
+        ctx.json({
+          tokenExpired: true,
+        }),
+      );
+    }
+
+    return res(
+      ctx.status(200),
+      ctx.delay(500),
+      ctx.json({
+        userId: 'mjsdo',
+        accessToken: 'accessToken',
+        refreshToken: 'refreshToken',
+        profileImageUrl:
+          'https://avatars.githubusercontent.com/u/79135734?s=96&v=4',
+      }),
+    );
+  },
+);
