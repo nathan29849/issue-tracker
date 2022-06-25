aa1a9e03bb25e288925bc7e93e0f895c67fa290e
Cola <bcad1591@gmail.com>
2022-06-25 19:28:06 +0900

    feat: #69 cookie util 추가

---
+++ FE/src/utils/cookie.ts
@@ -0,0 +1,50 @@
+interface Options {
+  [key: string]: string | boolean | Date;
+}
+
+export const setCookie = (
+  key: string,
+  value: string,
+  options: Options = {},
+) => {
+  const opts: Options = { path: '/', ...options };
+
+  if (opts.expires instanceof Date) {
+    opts.expires = opts.expires.toUTCString();
+  }
+
+  let cookieStr = `${encodeURIComponent(key)}=${encodeURIComponent(value)}`;
+
+  Object.entries(opts).forEach(([optsKey, optsValue]) => {
+    cookieStr += `; ${optsKey}`;
+    if (optsValue !== true) {
+      cookieStr += `=${optsValue}`;
+    }
+  });
+
+  document.cookie = cookieStr;
+};
+
+export const getCookie = (key: string): string | undefined => {
+  let result;
+  const { cookie } = document;
+
+  const cookieArr = cookie.split(';');
+
+  cookieArr.some(cookieStr => {
+    const cookieStrArr = cookieStr.split('=');
+    const [cookieKey, cookieValue] = [
+      cookieStrArr[0],
+      cookieStrArr.slice(1).join(''),
+    ];
+
+    if (cookieKey !== key) {
+      return false;
+    }
+
+    result = cookieValue;
+    return true;
+  });
+
+  return result;
+};
