b144e856f349352d72335fac3825e45a26b6c522
Cola <bcad1591@gmail.com>
2022-06-26 17:26:06 +0900

    feat: #69 Callback 페이지에서 useLogin 적용

--- FE/src/pages/Callback/index.tsx
+++ FE/src/pages/Callback/index.tsx
@@ -1,31 +1,15 @@
 import { css } from '@emotion/react';
 import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useSetRecoilState } from 'recoil';
-
-import { githubLogin } from '../../apis/login';
 
 import { Loader } from '@components/Indicator/Loader';
-import { userState } from '@recoil/atoms/user';
+import { useLogin } from '@hooks/useLogin';
 import theme from '@styles/theme';
 
 function Callback() {
-  const navigate = useNavigate();
   const { search } = window.location;
-  const setUser = useSetRecoilState(userState);
+  const login = useLogin();
 
   useEffect(() => {
-    const login = async (searchParams: string) => {
-      try {
-        const user = await githubLogin(searchParams);
-        console.log(user);
-        setUser(user);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        navigate('/');
-      }
-    };
     login(search);
   }, [search]);
 
