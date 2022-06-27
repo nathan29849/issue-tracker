d7fd9dbe7a304c0e3827237e3ae9aa639ffe3c20
Muffin <jinlog9@gmail.com>
2022-06-27 20:27:49 +0900

    feat: useDebounce 훅을 만들어서 검색 인풋창 글자수 체킹 적용하기

--- FE/src/constants/default.ts
+++ FE/src/constants/default.ts
@@ -1,2 +1,5 @@
 export const defaultProfileImageUrl =
   'https://www.stignatius.co.uk/wp-content/uploads/2020/10/default-user-icon.jpg';
+
+export const searchDebounceDelay = 300;
+export const limitedLengthSearchValue = 40;
