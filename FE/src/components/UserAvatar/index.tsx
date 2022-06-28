97c0a3233030b38d2d15db0e985272115ab8d7cf
Cola <bcad1591@gmail.com>
2022-06-29 04:29:03 +0900

    feat: #97 User Avatar에 fill props 추가
    이미지 대신 특정 색으로 배경을 칠할 때 사용

--- FE/src/components/UserAvatar/index.tsx
+++ FE/src/components/UserAvatar/index.tsx
@@ -8,6 +8,7 @@ interface Props {
   src: string;
   size?: 'sm' | 'lg';
   alt?: string;
+  fill?: string;
 }
 
 // T라는 리액트 엘리먼트의 props와 위에 정의한 Props의 props, as props를 가지는 type
@@ -17,11 +18,12 @@ const UserAvatar = <T extends React.ElementType = 'div'>({
   alt = '',
   size = 'sm',
   src,
+  fill,
   as,
   ...restProps
 }: UserAvatarProps<T>) => (
-  <S.UserAvatar as={as ?? 'div'} size={size} {...restProps}>
-    <S.Image src={src} alt={alt} />
+  <S.UserAvatar as={as ?? 'div'} size={size} fill={fill} {...restProps}>
+    {fill ? <S.Fill fill={fill} /> : <S.Image src={src} alt={alt} />}
   </S.UserAvatar>
 );
 
