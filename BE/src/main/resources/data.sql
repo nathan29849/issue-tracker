cc1383b917c4a6e9594f00a9523850b7e1007a30
nathan29849 <phs960418@gmail.com>
2022-06-28 16:09:01 +0900

    add: milestone 임시 데이터 추가

---
+++ BE/src/main/resources/data.sql
@@ -0,0 +1,15 @@
+-- Milestone
+insert into milestone(title, description, due_date, created_at, updated_at)
+values ('마일스톤1', '코카콜라 마일스톤1', '2022-08-20', '2020-01-12 00:00:01', '2020-02-12 00:00:01');
+
+insert into milestone(title, description, due_date, created_at, updated_at)
+values ('마일스톤2', '머핀 마일스톤2', '2022-05-20', '2020-01-12 00:00:01', '2020-02-12 00:00:01');
+
+insert into milestone(title, description, due_date, created_at, updated_at)
+values ('마일스톤3', '루니 마일스톤3', '2022-06-20', '2020-01-12 00:00:01', '2020-02-12 00:00:01');
+
+insert into milestone(title, description, due_date, created_at, updated_at)
+values ('마일스톤4', '나단 마일스톤4', '2022-08-25', '2020-01-12 00:00:01', '2020-02-12 00:00:01');
+
+insert into milestone(title, description, due_date, created_at, updated_at)
+values ('마일스톤5', 'team-30 마일스톤5', '2022-09-22', '2020-01-12 00:00:01', '2020-02-12 00:00:01');
