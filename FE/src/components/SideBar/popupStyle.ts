a11aff0b2958047d9e8d83dc63f2fe83f00ef58a
Cola <bcad1591@gmail.com>
2022-06-29 04:29:42 +0900

    feat: #97 LabelPanel 추가, 레이아웃 리팩토링
    - 팝업 내부에 InnerContainer를 두고, 리스트 아이템의 개수가 많아져서 높이가 커지면 스크롤을 사용하도록 변경 -> width는 Popup 컴포넌트의 width를 키우면 됩니다.

--- FE/src/components/SideBar/popupStyle.ts
+++ FE/src/components/SideBar/popupStyle.ts
@@ -3,21 +3,30 @@ import styled from '@emotion/styled';
 import { flexbox, typoSmall } from '@styles/mixin';
 
 export const PopupContainer = styled.div`
+  z-index: ${({ theme }) => theme.zIndex.sideBar.popup};
   position: absolute;
   top: 2rem;
   right: -0.5rem;
 `;
 
+export const InnerContainer = styled.div`
+  max-height: 18rem;
+  overflow-y: scroll;
+`;
+
 export const Header = styled.header`
   background-color: ${({ theme }) => theme.color.background};
+  border-bottom: 1px solid ${({ theme }) => theme.color.line};
+  position: sticky;
+  top: 0;
 `;
 
 export const List = styled.ul``;
 
 export const ItemCommon = styled.li<{ selected: boolean }>`
+  ${flexbox({ ai: 'center' })}
   height: 2.75rem;
   padding: 0 1rem;
-  border-top: 1px solid ${({ theme }) => theme.color.line};
   cursor: pointer;
   background-color: ${({ theme }) => theme.color.offWhite};
   color: ${({ theme, selected }) =>
@@ -27,19 +36,25 @@ export const ItemCommon = styled.li<{ selected: boolean }>`
   &:hover {
     background-color: ${({ theme }) => theme.color.background};
   }
+
+  &:not(:first-child) {
+    border-top: 1px solid ${({ theme }) => theme.color.line};
+  }
 `;
 
-export const AssigneeItem = styled(ItemCommon)`
-  ${flexbox({ ai: 'center' })}
-`;
-
-export const AvatarWrapper = styled.div`
-  flex-shrink: 0;
-`;
-
-export const AssigneeName = styled.p`
+export const TitleCommon = styled.p`
   ${typoSmall(400)};
   margin-left: 0.5rem;
   flex-grow: 1;
   word-break: break-all;
 `;
+
+export const AssigneeItem = styled(ItemCommon)``;
+
+export const AvatarWrapper = styled.div`
+  flex-shrink: 0;
+`;
+
+export const AssigneeName = styled(TitleCommon)``;
+
+export const LabelTitle = styled(TitleCommon)``;
