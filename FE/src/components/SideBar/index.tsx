b4a47fbd67d4edc3a38cbd9b80c6b6b94aeaf25f
Cola <bcad1591@gmail.com>
2022-06-30 01:11:13 +0900

    feat: #108 SideBar 컴포넌트 추가

--- FE/src/components/SideBar/index.tsx
+++ FE/src/components/SideBar/index.tsx
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useQuery } from 'react-query';
 
 import AssigneePanel from './AssigneePanel';
 import LabelPanel from './LabelPanel';
 import MileStonePanel from './MileStonePanel';
 import * as S from './style';
 
+import { getLabels } from '@apis/label';
+import { getMileStones } from '@apis/milestone';
+import { getUsers } from '@apis/user';
+import {
+  useAssigneePanel,
+  useLabelPanel,
+  useMileStonePanel,
+} from '@components/SideBar/context';
+
 const Container: React.FC<{ children: React.ReactNode }> = ({ children }) => (
   <S.Container>{children}</S.Container>
 );
+
+export const SideBar = () => {
+  const { data: userData } = useQuery(['users'], getUsers);
+  const { data: labelData } = useQuery(['labels'], getLabels);
+  const { data: milestoneData } = useQuery(['milestones'], getMileStones);
+  const { initPanel: initAssigneePanel } = useAssigneePanel();
+  const { initPanel: initLabelPanel } = useLabelPanel();
+  const { initPanel: initMileStonePanel } = useMileStonePanel();
+
+  useEffect(() => {
+    initAssigneePanel(userData);
+    initLabelPanel(labelData);
+    initMileStonePanel(milestoneData);
+  }, [userData, labelData, milestoneData]);
+
+  return (
+    <Container>
+      <AssigneePanel allowDuplicates />
+      <LabelPanel allowDuplicates />
+      <MileStonePanel />
+    </Container>
+  );
+};
 
 export default {
   Container,
