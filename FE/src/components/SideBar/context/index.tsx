cd0793868838db4aed9c2597d864ffa1fdbb4053
Cola <bcad1591@gmail.com>
2022-06-29 16:05:48 +0900

    feat: #106 Compose를 활용하여 3개 Provider를 합성한 SideBarProvider 추가

--- FE/src/components/SideBar/context/index.tsx
+++ FE/src/components/SideBar/context/index.tsx
@@ -1,20 +1,43 @@
+import React from 'react';
+
+import { AssigneePanelProvider } from './AssigneePanel';
+import { LabelPanelProvider } from './LabelPanel';
+import { MileStonePanelProvider } from './MileStonePanel';
+
+import Compose from '@utils/Compose';
+
 export {
   useAssigneePanel,
   useAssigneePanelState,
   useSetAssigneePanelState,
-  AssigneePanelProvider,
 } from './AssigneePanel';
 
 export {
   useLabelPanel,
   useLabelPanelState,
   useSetLabelPanelState,
-  LabelPanelProvider,
 } from './LabelPanel';
 
 export {
   useMileStonePanel,
   useMileStonePanelState,
   useSetMileStonePanelState,
-  MileStonePanelProvider,
 } from './MileStonePanel';
+
+export { AssigneePanelProvider, LabelPanelProvider, MileStonePanelProvider };
+
+export const SideBarProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => (
+  <Compose
+    components={[
+      AssigneePanelProvider,
+      LabelPanelProvider,
+      MileStonePanelProvider,
+    ]}
+  >
+    {children}
+  </Compose>
+);
