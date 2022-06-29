74ae8183d03de77510b8db5a0fbd13cd0bae6e7f
Cola <bcad1591@gmail.com>
2022-06-29 11:04:27 +0900

    feat: #97 milestone fetch 함수 추가

---
+++ FE/src/apis/milestone.ts
@@ -0,0 +1,10 @@
+import { MileStone } from '@type/milestone';
+
+export const getMileStones = async (): Promise<MileStone[]> => {
+  const response = await fetch(`${process.env.TEAM30_BASE_URL}/api/milestones`);
+  const data = await response.json();
+
+  // TODO: 에러핸들링
+
+  return data;
+};
