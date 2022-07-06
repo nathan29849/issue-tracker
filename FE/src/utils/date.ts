830dc27dfa7c311f75a1c78ff0d244e765d1b571
Muffin <jinlog9@gmail.com>
2022-07-06 17:19:20 +0900

    refactor: checkDueDate 함수 정규 식 수정

--- FE/src/utils/date.ts
+++ FE/src/utils/date.ts
@@ -1,4 +1,4 @@
 export const checkDueDate = (dueDate: string) => {
-  const pattern = /[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])/;
+  const pattern = /^(19|20)\d{2}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[0-1])$/;
   return pattern.test(dueDate);
 };
