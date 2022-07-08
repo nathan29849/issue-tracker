33b5fc19af16e2b754fd207745a6c6c6cf2043df
Cola <bcad1591@gmail.com>
2022-07-09 04:01:19 +0900

    refactor: #139 getIssues mock api 분리

--- FE/src/mocks/handlers/GetIssues.js
+++ FE/src/mocks/handlers/GetIssues.js
@@ -1,114 +1,65 @@
 import { rest } from 'msw';
 
-const getIssue = rest.get(
-  `${process.env.TEAM30_BASE_URL}/api/issues`,
-  (req, res, ctx) =>
-    res(
-      ctx.status(200),
-      ctx.delay(1000),
-      ctx.json([
-        {
-          id: 1,
-          status: 'OPEN',
-          title: '로그인 페이지 기능 작업',
-          manager: ['muffin', 'cola'],
-          labels: [
-            {
-              id: 1,
-              title: 'BUG',
-              backgroundColor: '#fe2',
-              textColor: 'BLACK',
-            },
-          ],
-          milestone: {
-            id: 1,
-            title: '1주차 마일스톤',
-            description: '마일스톤',
-          },
-          author: [
-            {
-              userId: 'muffin',
-              profileImageUrl:
-                'https://avatars.githubusercontent.com/u/45479309?v=4',
-            },
-          ],
-          assignees: [
-            {
-              userId: 'mjsdo',
-              profileImageUrl:
-                'https://avatars.githubusercontent.com/u/79135734?s=96&v=4',
-            },
-          ],
-          createdAt: '2022-12-17T03:24:00',
-        },
-        {
-          id: 2,
-          status: 'CLOSED',
-          title: '레이블 페이지 기능 작업',
-          manager: ['muffin', 'cola'],
-          labels: [
-            {
-              id: 1,
-              title: 'BUG',
-              backgroundColor: '#fe2',
-              textColor: 'BLACK',
-            },
-          ],
-          milestone: {
-            id: 1,
-            title: '1주차 마일스톤',
-            description: '마일스톤',
-          },
-          author: [
-            {
-              userId: 'muffin',
-              profileImageUrl:
-                'https://avatars.githubusercontent.com/u/45479309?v=4',
-            },
-          ],
-          assignees: [
-            {
-              userId: 'mjsdo',
-              profileImageUrl:
-                'https://avatars.githubusercontent.com/u/79135734?s=96&v=4',
-            },
-          ],
-          createdAt: '2022-12-17T03:24:00',
-        },
-        {
-          id: 3,
-          status: 'OPEN',
-          title: '마일스톤 페이지 기능 작업',
-          manager: ['muffin', 'cola'],
-          labels: [
-            {
-              id: 1,
-              title: 'BUG',
-              backgroundColor: '#fe2',
-              textColor: 'BLACK',
-            },
-          ],
-          milestone: {
-            id: 1,
-            title: '1주차 마일스톤',
-            description: '마일스톤',
-          },
-          author: [
-            {
-              userId: 'muffin',
-              profileImageUrl:
-                'https://avatars.githubusercontent.com/u/45479309?v=4',
-            },
-          ],
-          assignees: [
-            {
-              userId: 'mjsdo',
-              profileImageUrl:
-                'https://avatars.githubusercontent.com/u/79135734?s=96&v=4',
-            },
-          ],
-          createdAt: '2022-12-17T03:24:00',
-        },
-      ]),
-    ),
+export const getIssues = rest.get('/issue', (req, res, ctx) =>
+  res(
+    ctx.status(200),
+    ctx.delay(1000),
+    ctx.json([
+      {
+        id: 1,
+        number: 3,
+        status: 'open',
+        title: '로그인 페이지 기능 작업',
+        manager: ['muffin', 'cola'],
+        labels: ['feat', 'sub', 'bug'],
+        milestone: '1주차 이슈트래커',
+        author: [{ name: 'muffin', iamgeUrl: 'http://xxx' }],
+        date: '2022-12-17T03:24:00',
+      },
+      {
+        id: 2,
+        number: 5, // 이슈 고유값
+        status: 'close',
+        title: '로그인 기능 작업',
+        manager: ['muffin', 'cola'],
+        labels: ['feat', 'sub', 'bug'],
+        milestone: '2주차 이슈트래커',
+        author: [{ name: 'cola', iamgeUrl: 'http://xxx' }],
+        date: '2022-12-17T03:24:00',
+      },
+      {
+        id: 3,
+        number: 2, // 이슈 고유값
+        status: 'close',
+        title: '리팩토링 작업',
+        manager: ['muffin', 'cola'],
+        labels: ['feat', 'sub', 'bug'],
+        milestone: '3주차 이슈트래커',
+        author: [{ name: 'muffin', iamgeUrl: 'http://xxx' }],
+        date: '2022-12-17T03:24:00',
+      },
+      {
+        id: 4,
+        number: 10,
+        status: 'open',
+        title: '레이블 페이지 기능 작업',
+        manager: ['muffin'],
+        labels: ['feat', 'sub'],
+        milestone: '3주차 이슈트래커',
+        author: [{ name: 'muffin', iamgeUrl: 'http://xxx' }],
+        date: '2022-12-17T03:24:00',
+      },
+      {
+        id: 5,
+        number: 12,
+        status: 'open',
+        title: '이슈 생성 페이지 기능 작업',
+        manager: ['cola'],
+        labels: ['feat', 'sub'],
+        milestone: '3주차 이슈트래커',
+        author: [{ name: 'cola', iamgeUrl: 'http://xxx' }],
+        date: '2022-12-17T03:24:00',
+      },
+    ]),
+  ),
 );
