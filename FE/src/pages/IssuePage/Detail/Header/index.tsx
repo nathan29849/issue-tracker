36c1abacd4402583e2b6e31975561099db2245a8
Cola <bcad1591@gmail.com>
2022-07-09 07:11:59 +0900

    comment: #139 코멘트 추가
    issueStatus에 더해서 issueStatusEditor도 API 응답에 포함시킬 수 있는지 Backend와 상의하기.

--- FE/src/pages/IssuePage/Detail/Header/index.tsx
+++ FE/src/pages/IssuePage/Detail/Header/index.tsx
@@ -67,6 +67,7 @@ export const DetailHeader: React.FC = memo(() => {
         />
       )}
       <S.MetaData>
+        {/* NOTE: Backend와 상의 후 재개  */}
         <IssueStatusInfo
           issueStatus={issueDetailData.issueStatus}
           updatedAt={issueDetailData.updatedAt}
