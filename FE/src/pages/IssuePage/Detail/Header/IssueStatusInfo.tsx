10c249429a1ec40930a404e6fef50c6ed1b67563
Cola <bcad1591@gmail.com>
2022-07-09 07:17:47 +0900

    feat: #139 getPredicateByIssueStatus 추가
    파라미터로 전달되는 이슈의 상태에 따라 적절한 info의 서술어를 반환.

--- FE/src/pages/IssuePage/Detail/Header/IssueStatusInfo.tsx
+++ FE/src/pages/IssuePage/Detail/Header/IssueStatusInfo.tsx
@@ -11,6 +11,19 @@ interface Props {
   updatedAt: string;
 }
 
+const getPredicateByIssueStatus = (issueStatus: IssueStatusType) => {
+  switch (issueStatus) {
+    case 'OPEN':
+      return '열렸습니다';
+    case 'REOPEN':
+      return '다시 열렸습니다';
+    case 'CLOSED':
+      return '닫혔습니다';
+    default:
+      throw new Error(`Unknown IssueStatus, ${issueStatus}`);
+  }
+};
+
 const IssueStatusInfo: React.FC<Props> = ({ issueStatus, updatedAt }) => {
   const isIssueClosed = issueStatus === 'CLOSED';
   const timeDiff = useMemo(
@@ -18,13 +31,15 @@ const IssueStatusInfo: React.FC<Props> = ({ issueStatus, updatedAt }) => {
     [updatedAt],
   );
 
+  const predicate = getPredicateByIssueStatus(issueStatus);
+
   return (
     <>
       <S.IssueStatus>
         <IssueLabel closed={isIssueClosed} />
       </S.IssueStatus>
       <S.IssueStatusDetail>
-        이 이슈가 {timeDiff}에 X에 의해 열렸습니다
+        이 이슈가 {timeDiff}에 X에 의해 {predicate}
       </S.IssueStatusDetail>
     </>
   );
