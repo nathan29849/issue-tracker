7ad44f8a7c21354465d5ce56862c267772a541c8
Cola <bcad1591@gmail.com>
2022-07-10 01:30:07 +0900

    feat: #143 코멘트 등록 버튼 이벤트 핸들러 추가 및 등록 버튼 컴포넌트 분리

--- FE/src/pages/IssuePage/Detail/Main/CommentForm.tsx
+++ FE/src/pages/IssuePage/Detail/Main/CommentForm.tsx
@@ -1,4 +1,4 @@
-import React, { memo } from 'react';
+import React, { memo, useCallback } from 'react';
 
 import * as S from './style';
 
@@ -7,22 +7,61 @@ import I from '@components/Icons';
 import { Textarea } from '@components/Input';
 import { useInput } from '@hooks/useInput';
 import { useUserState } from '@hooks/useIsLoggedIn';
+import { usePostComment } from '@hooks/usePostComment';
+import { CreateCommentFormLoader } from '@pages/IssuePage/Detail/Main/CommentFormLoader';
 import UserAvatarLayer from '@pages/IssuePage/Detail/Main/UserAvatarLayer';
+import { ButtonClickEventHandler } from '@type/eventHandler';
 
-export const CreateCommentForm = () => {
+export const CreateCommentForm: React.FC<{ issueId: string }> = ({
+  issueId,
+}) => {
   const { profileImageUrl } = useUserState();
   const comment = useInput();
+  const { isLoading: postCommentLoading, mutateAsync: mutateIssueComments } =
+    usePostComment(issueId);
+
+  const handleClickCreateButton = useCallback(async () => {
+    if (!comment.value.length) {
+      return;
+    }
+
+    // 항상 status 값을 리턴한다.
+    // Post 실패시, Input 값을 초기화하지 않는다.
+    const { status } = await mutateIssueComments(comment.value);
+    if (status === 'success') {
+      comment.setValue('');
+      return;
+    }
+
+    globalThis.alert('코멘트 등록에 실패했습니다.');
+  }, [comment.value]);
 
   return (
     <S.IssueCommentForm>
-      <S.TextareaLayer>
-        <UserAvatarLayer profileImage={profileImageUrl} />
-        <Textarea width="100%" {...comment} />
-      </S.TextareaLayer>
-      <Button disabled={!comment.value.length}>
-        <I.Plus />
-        코멘트 작성
-      </Button>
+      {postCommentLoading ? (
+        <CreateCommentFormLoader />
+      ) : (
+        <>
+          <S.TextareaLayer>
+            <UserAvatarLayer profileImage={profileImageUrl} />
+            <Textarea width="100%" {...comment} />
+          </S.TextareaLayer>
+          <CreateButton
+            disabled={!comment.value.length}
+            onClick={handleClickCreateButton}
+          />
+        </>
+      )}
     </S.IssueCommentForm>
   );
 };
+
+const CreateButton: React.FC<{
+  disabled: boolean;
+  onClick: ButtonClickEventHandler;
+}> = memo(({ disabled, onClick }) => (
+  <Button disabled={disabled} onClick={onClick}>
+    <I.Plus />
+    코멘트 작성
+  </Button>
+));
